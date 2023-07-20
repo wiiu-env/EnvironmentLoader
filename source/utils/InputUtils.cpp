2b9a13e8f0d1332481aea07c7c1083e143039621
Maschell <Maschell@gmx.de>
2023-07-20 10:05:52 +0200

    Make input reading more reliable

--- source/utils/InputUtils.cpp
+++ source/utils/InputUtils.cpp
@@ -1,4 +1,6 @@
 #include "InputUtils.h"
+#include "logger.h"
+#include <coreinit/thread.h>
 #include <padscore/kpad.h>
 #include <padscore/wpad.h>
 #include <vpad/input.h>
@@ -97,11 +99,15 @@ InputUtils::InputData InputUtils::getControllerInput() {
     InputData inputData{};
     VPADStatus vpadStatus{};
     VPADReadError vpadError = VPAD_READ_UNINITIALIZED;
-    if (VPADRead(VPAD_CHAN_0, &vpadStatus, 1, &vpadError) > 0 && vpadError == VPAD_READ_SUCCESS) {
-        inputData.trigger = vpadStatus.trigger;
-        inputData.hold    = vpadStatus.hold;
-        inputData.release = vpadStatus.release;
-    }
+    do {
+        if (VPADRead(VPAD_CHAN_0, &vpadStatus, 1, &vpadError) > 0 && vpadError == VPAD_READ_SUCCESS) {
+            inputData.trigger = vpadStatus.trigger;
+            inputData.hold    = vpadStatus.hold;
+            inputData.release = vpadStatus.release;
+        } else {
+            OSSleepTicks(OSMillisecondsToTicks(1));
+        }
+    } while (vpadError == VPAD_READ_NO_SAMPLES);
 
     KPADStatus kpadStatus{};
     KPADError kpadError = KPAD_ERROR_UNINITIALIZED;
